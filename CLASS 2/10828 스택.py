4cbf97a79e898e8dbabf77324126ca5b51d96663
wingunkh <99gusrms@naver.com>
2023-03-31 21:38:38 +0900

    모범 답안

--- CLASS 2/10828 스택.py
+++ CLASS 2/10828 스택.py
@@ -2,24 +2,20 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-
 stack = []
+
 for i in range(n):
     cmd = input().split()    
 
     if cmd[0] == "push":
         stack.append(cmd[1])
     elif cmd[0] == "pop":
-        if len(stack) > 0:
-            print(stack[len(stack) - 1])
-            stack.pop()
-        else:
-            print(-1)
+        print(stack.pop(-1) if stack else -1)
     elif cmd[0] == "size":
         print(len(stack))
     elif cmd[0] == "empty":
         print(1 if len(stack) == 0 else 0)
     elif cmd[0] == "top":
-        print(stack[len(stack) - 1] if len(stack) > 0 else -1)
+        print(stack[-1] if stack else -1)
     else:
         break
