70ca062d9b284c5aa1799fabaec16deab45b642f
wingunkh <99gusrms@naver.com>
2023-05-14 00:29:33 +0900

    정답

--- CLASS 2/2775 부녀회장이 될테야.py
+++ CLASS 2/2775 부녀회장이 될테야.py
@@ -1,11 +1,11 @@
 def func(a: int, b: int):
-    f0 = [i for i in range(1, b+1)]
+    f = [i for i in range(1, b+1)]
 
     for i in range(a):
         for j in range(1, b):
-            f0[j] += f0[j - 1]
+            f[j] += f[j - 1]
 
-    print(f0[-1])
+    print(f[-1])
             
 t = int(input())
 
