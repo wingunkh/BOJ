dddacfadfc5d43181481ee7a34e282b185ea5da1
wingunkh <99gusrms@naver.com>
2023-07-09 23:33:25 +0900

    정답

---
+++ CLASS 3/2178 미로 탐색.py
@@ -0,0 +1,28 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def BFS(row, col):
+    dr = [-1, 1, 0, 0]
+    dc = [0, 0, -1, 1]
+
+    q = deque()
+    q.append((row, col))
+
+    while q:
+        row, col = q.popleft()
+
+        for i in range(4):
+            nr = row + dr[i]
+            nc = col + dc[i]
+
+            if 0 <= nr < N and 0 <= nc < M and maze[nr][nc] == 1:
+                q.append((nr, nc))
+                maze[nr][nc] = maze[row][col] + 1
+    return maze[N-1][M-1]
+
+N, M = map(int, input().split())
+maze = [list(map(int, input().strip())) for _ in range(N)]
+cnt = 0
+
+print(BFS(0,0))
