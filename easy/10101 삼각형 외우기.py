3d0adc7e9a4015dde933c20d613050200d92b4d6
wingunkh <99gusrms@naver.com>
2023-11-22 14:02:17 +0900

    easy

--- easy/10101 삼각형 외우기.py
+++ easy/10101 삼각형 외우기.py
@@ -6,7 +6,9 @@ if a + b + c != 180:
     print("Error")
 elif a == b == c:
     print("Equilateral")
-elif a != b and b != c and a != c: # a != b != c 조건식과 다르다.
+elif a != b and b != c and a != c:
+    # a != b != c 는 a != b and b != c와 같다.
+    # 그러므로 a != b and b != c and a != c가 옳다.
     print("Scalene")
 else:
     print("Isosceles")
