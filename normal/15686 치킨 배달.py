a72b85a5d704696c044a3b4de5de42664db2ae3a
wingunkh <99gusrms@naver.com>
2023-12-06 15:04:38 +0900

    normal

--- normal/15686 치킨 배달.py
+++ normal/15686 치킨 배달.py
@@ -3,29 +3,29 @@ input = sys.stdin.readline
 
 def dfs(index, depth):
     global result
-    
+
     if depth == m:
         buff = 0
         
         for r1, c1 in house:
             distance = sys.maxsize
-            
+
             for i in range(len(chicken)):
                 if visited[i]:
-                    distance = min(distance, abs(r1 - chicken[i][0]) + abs(c1 - chicken[i][1]))
+                    r2, c2 = chicken[i]
+                    distance = min(distance, abs(r1 - r2) + abs(c1 - c2))
 
             buff += distance
 
         result = min(result, buff)
 
         return
-                    
+
     for i in range(index, len(chicken)):
-        if not visited[i]:
-            visited[i] = True
-            dfs(i+1, depth+1)
-            visited[i] = False
-        
+        visited[i] = True
+        dfs(i+1, depth+1)
+        visited[i] = False
+            
 n, m = map(int, input().split())
 a = [list(map(int, input().split())) for _ in range(n)]
 house = []
